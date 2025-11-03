d64b7d816e22a9b6cd151274cc040b073453190e
srikanthdevabattula <srikanthdevabathula@gmail.com>
2025-11-03 20:34:24 +0530

    added cocktails

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger,SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
@@ -12,7 +13,7 @@ function App() {
     <main>
       <Navbar />
       <Hero />
-     
+     <Cocktails/>
     </main>
   
   )
