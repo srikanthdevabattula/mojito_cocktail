e4c52b7433ca0e1942717cc935644491336a28e8
srikanthdevabattula <srikanthdevabathula@gmail.com>
2025-11-03 22:01:05 +0530

    added full

--- constants/index.js
+++ constants/index.js
@@ -120,17 +120,17 @@ const socials = [
  {
 	name: "Instagram",
 	icon: "/images/insta.png",
-	url: "#",
+	url: "https://www.instagram.com/",
  },
  {
 	name: "X (Twitter)",
 	icon: "/images/x.png",
-	url: "#",
+	url: "https://www.teitter.com",
  },
  {
 	name: "Facebook",
 	icon: "/images/fb.png",
-	url: "#",
+	url: "https://www.fb.com",
  },
 ];
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -82,7 +82,7 @@ const Hero = () => {
           className="right-leaf"
         />
         <div className="body">
-          {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+          <img src="/images/arrow.png" alt="arrow" className="arrow" />
 
           <div className="content">
             <div className="space-y-5 hidden md:block">
